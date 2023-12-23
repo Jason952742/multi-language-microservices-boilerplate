ec4fecf8693b24875f67ff380bcabe9022b979aa
jason <jason@jaasun.com>
2023-12-23 18:35:09 +0800

    member service: update to quarkus 3.6.4

--- microservices/member/build.gradle.kts
+++ microservices/member/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.allopen") version "1.9.10"
-    kotlin("plugin.noarg") version "1.9.10"
-    kotlin("plugin.serialization") version "1.9.10"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.allopen") version "1.9.21"
+    kotlin("plugin.noarg") version "1.9.21"
+    kotlin("plugin.serialization") version "1.9.21"
 
     id("io.quarkus")
     id("java")
@@ -21,57 +21,57 @@ val quarkusPlatformVersion: String by project
 dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
 
-    implementation("io.quarkus:quarkus-kotlin:2.16.4.Final")
+    implementation("io.quarkus:quarkus-kotlin:3.6.4.Final")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("io.smallrye.reactive:mutiny-kotlin:2.1.0")
 
-    implementation("io.quarkus:quarkus-resteasy-reactive:2.16.4.Final")
-    implementation("io.quarkus:quarkus-resteasy-reactive-qute:2.16.4.Final")
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson:2.16.4.Final")
-    implementation("io.quarkus:quarkus-resteasy-reactive-kotlin-serialization:2.16.4.Final")
+    implementation("io.quarkus:quarkus-resteasy-reactive:3.6.4.Final")
+    implementation("io.quarkus:quarkus-resteasy-reactive-qute:3.6.4.Final")
+    implementation("io.quarkus:quarkus-resteasy-reactive-jackson:3.6.4.Final")
+    implementation("io.quarkus:quarkus-resteasy-reactive-kotlin-serialization:3.6.4.Final")
 
-    implementation("io.quarkus:quarkus-rest-client-reactive-jackson:2.16.4.Final")
-    implementation("io.quarkus:quarkus-rest-client-reactive-kotlin-serialization:2.16.4.Final")
+    implementation("io.quarkus:quarkus-rest-client-reactive-jackson:3.6.4.Final")
+    implementation("io.quarkus:quarkus-rest-client-reactive-kotlin-serialization:3.6.4.Final")
 
-    implementation("io.quarkus:quarkus-hibernate-envers:2.16.4.Final")
-    implementation("io.quarkus:quarkus-hibernate-reactive-panache-kotlin:2.16.4.Final")
-    implementation("io.quarkus:quarkus-reactive-pg-client:2.16.4.Final")
-    implementation("io.quarkus:quarkus-hibernate-validator:2.16.4.Final")
+    implementation("io.quarkus:quarkus-hibernate-envers:3.6.4.Final")
+    implementation("io.quarkus:quarkus-hibernate-reactive-panache-kotlin:3.6.4.Final")
+    implementation("io.quarkus:quarkus-reactive-pg-client:3.6.4.Final")
+    implementation("io.quarkus:quarkus-hibernate-validator:3.6.4.Final")
 
-    implementation("io.quarkus:quarkus-smallrye-openapi:2.16.4.Final")
-    implementation("io.quarkus:quarkus-smallrye-health:2.16.4.Final")
-    implementation("io.quarkus:quarkus-smallrye-context-propagation:2.16.4.Final")
-    implementation("io.quarkus:quarkus-smallrye-reactive-messaging:2.16.4.Final")
-    implementation("io.quarkus:quarkus-smallrye-jwt:2.16.4.Final")
-    implementation("io.quarkus:quarkus-smallrye-jwt-build:2.16.4.Final")
-    implementation("io.quarkus:quarkus-smallrye-fault-tolerance:2.16.4.Final")
+    implementation("io.quarkus:quarkus-smallrye-openapi:3.6.4.Final")
+    implementation("io.quarkus:quarkus-smallrye-health:3.6.4.Final")
+    implementation("io.quarkus:quarkus-smallrye-context-propagation:3.6.4.Final")
+    implementation("io.quarkus:quarkus-smallrye-reactive-messaging:3.6.4.Final")
+    implementation("io.quarkus:quarkus-smallrye-jwt:3.6.4.Final")
+    implementation("io.quarkus:quarkus-smallrye-jwt-build:3.6.4.Final")
+    implementation("io.quarkus:quarkus-smallrye-fault-tolerance:3.6.4.Final")
 
-    implementation("io.quarkus:quarkus-redis-client:2.16.4.Final")
-    implementation("io.quarkus:quarkus-redis-cache:2.16.4.Final")
-    implementation("io.quarkus:quarkus-cache:2.16.4.Final")
+    implementation("io.quarkus:quarkus-redis-client:3.6.4.Final")
+    implementation("io.quarkus:quarkus-redis-cache:3.6.4.Final")
+    implementation("io.quarkus:quarkus-cache:3.6.4.Final")
     implementation("io.quarkiverse.neo4j:quarkus-neo4j:3.5.0")
 
-    implementation("io.quarkus:quarkus-websockets:2.16.4.Final")
-    implementation("io.quarkus:quarkus-websockets-client:2.16.4.Final")
+    implementation("io.quarkus:quarkus-websockets:3.6.4.Final")
+    implementation("io.quarkus:quarkus-websockets-client:3.6.4.Final")
 
     implementation("io.quarkiverse.openapi.generator:quarkus-openapi-generator:2.2.14")
-    implementation("io.quarkus:quarkus-mutiny:2.16.4.Final")
+    implementation("io.quarkus:quarkus-mutiny:3.6.4.Final")
     implementation("io.quarkus:quarkus-grpc:3.2.1.Final")
-    implementation("io.quarkus:quarkus-quartz:2.16.4.Final")
+    implementation("io.quarkus:quarkus-quartz:3.6.4.Final")
 
-    implementation("io.quarkus:quarkus-jackson:2.16.4.Final")
+    implementation("io.quarkus:quarkus-jackson:3.6.4.Final")
+    implementation("io.quarkus:quarkus-scheduler:3.6.4.Final")
     implementation("io.github.microcks.quarkus:quarkus-microcks:0.1.3")
-    implementation("io.quarkus:quarkus-arc:2.16.4.Final")
+    implementation("io.quarkus:quarkus-arc:3.6.4.Final")
     implementation("commons-codec:commons-codec:1.15")
     implementation("io.github.novacrypto:BIP39:2019.01.27")
 
-    testImplementation("io.quarkus:quarkus-junit5:2.16.4.Final")
+    testImplementation("io.quarkus:quarkus-junit5:3.6.4.Final")
     testImplementation("io.rest-assured:rest-assured:5.3.0")
     testImplementation("org.awaitility:awaitility:4.2.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("io.rest-assured:kotlin-extensions:5.3.0")
-    testImplementation("io.quarkus:quarkus-test-keycloak-server:2.16.4.Final")
 }
 
 group = "org.acme"
