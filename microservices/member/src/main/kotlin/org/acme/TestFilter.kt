f7ce55115968d3d7e59d8a970b0305d77814d658
jason <jason@jaasun.com>
2023-12-25 02:12:31 +0800

    member service: log filter

---
+++ microservices/member/src/main/kotlin/org/acme/TestFilter.kt
@@ -0,0 +1,16 @@
+package org.acme
+
+import io.quarkus.logging.LoggingFilter
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.util.logging.Filter
+import java.util.logging.LogRecord
+
+
+@LoggingFilter(name = "my-filter")
+class TestFilter(
+    @param:ConfigProperty(name = "my-filter.legacy-gRPC") private val part: String
+) : Filter {
+    override fun isLoggable(record: LogRecord): Boolean {
+        return !record.message.contains(part) && !record.message.contains("Installed features: [")
+    }
+}
