ccdd8fb666dcf4a47d28ccfddb885f75cf5abd4d
jason <jason@jaasun.com>
2023-12-26 02:45:41 +0800

    referral service: add tokio demo

--- microservices/referral/infras/src/config/rabbitmq.rs
+++ microservices/referral/infras/src/config/rabbitmq.rs
@@ -1,5 +1,5 @@
-use std::sync::Arc;
-use lapin::{options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection, ConnectionProperties, Channel, Queue};
+use std::time::Duration;
+use lapin::{options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection, ConnectionProperties, Channel, Queue, Consumer};
 use lapin::message::DeliveryResult;
 use tracing::info;
 
@@ -8,101 +8,85 @@ pub struct Rabbitmq;
 
 impl Rabbitmq {
     pub async fn connection() -> Connection {
-        let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://rabbit:rabbitpassword@127.0.0.1:5672/%2f".into());
-        let conn = Connection::connect(&addr, ConnectionProperties::default()).await.expect("connection error");
-        info!("CONNECTED");
-        conn
+        let uri = "amqp://rabbit:rabbitpassword@127.0.0.1:5672/%2f";
+        // Use tokio executor and reactor.
+        // At the moment the reactor is only available for unix.
+        let options = ConnectionProperties::default()
+            .with_executor(tokio_executor_trait::Tokio::current())
+            .with_reactor(tokio_reactor_trait::Tokio);
+        let connection = Connection::connect(uri.into(), options)
+            .await.expect("Connection failed");
+        info!("RABBITMQ CONNECTED");
+        connection
     }
 
     pub async fn channel(conn: &Connection) -> Channel {
-        conn.create_channel().await.expect("create_channel")
+        conn.create_channel().await.expect("create channel failed")
     }
 
     pub async fn queue(channel: &Channel, queue_name: &str) -> Queue {
-        let queue = channel
+        channel
             .queue_declare(queue_name, QueueDeclareOptions::default(), FieldTable::default())
-            .await.expect("queue_declare");
-        info!(?queue, "Declared queue");
-        queue
+            .await.expect("queue_declare failed")
     }
 
-    pub async fn longlive_consume() {}
-
-    pub async fn consume(conn: &Connection, queue: &str, consumer_tag: &str) -> () {
-        // receive channel
-        let channel_receive = Rabbitmq::channel(&conn).await;
-        let consumer_tag = Arc::new(consumer_tag.to_string());
-
-        let _ = &channel_receive
-            .basic_consume(queue, &consumer_tag, BasicConsumeOptions::default(), FieldTable::default())
-            .await.expect("basic_consume")
-            .set_delegate(move |delivery: DeliveryResult| {
-                let channel = channel_receive.clone();
-                let consumer_tag = Arc::clone(&consumer_tag);
-
-                async move {
-                    info!(message=?delivery, "consume received message: ");
-                    if let Ok(Some(delivery)) = delivery {
-
-                        delivery
-                            .ack(BasicAckOptions::default())
-                            .await.expect("basic_ack");
-
-                        channel
-                            .basic_cancel(&consumer_tag, BasicCancelOptions::default())
-                            .await.expect("basic_cancel");
-                    }
-                }
-            });
+    pub async fn consumer(channel: &Channel, queue: &str, consumer_tag: &str) -> Consumer {
+        channel
+            .basic_consume(queue, consumer_tag, BasicConsumeOptions::default(), FieldTable::default())
+            .await.expect("consumer create failed")
     }
 
-    pub async fn publish(channel: &Channel, exchange: &str, routing_key: &str, payload: &[u8]) -> Confirmation {
+    pub async fn send(channel: &Channel, exchange: &str, routing_key: &str, payload: &[u8]) -> Confirmation {
+        info!("will publish");
         channel
             .basic_publish(exchange, routing_key, BasicPublishOptions::default(), payload, BasicProperties::default())
-            .await.expect("basic_publish")
-            .await.expect("publisher-confirms")
+            .await.expect("basic_publish failed")
+            .await.expect("publisher-confirms failed")
     }
 }
 
 
 #[tokio::test]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "info");
-    }
+    Ok(())
+}
 
-    tracing_subscriber::fmt::init();
+#[tokio::test]
+async fn tokio_test() -> Result<(), Box<dyn std::error::Error>> {
+    let connection = Rabbitmq::connection().await;
+    let channel = Rabbitmq::channel(&connection).await;
+    let _queue = Rabbitmq::queue(&channel, "queue_test").await;
+    let consumer = Rabbitmq::consumer(&channel, "queue_test", "tag_foo").await;
 
-    async_global_executor::block_on(async {
-        let conn = Rabbitmq::connection().await;
+    consumer.set_delegate(move |delivery: DeliveryResult| async move {
+        let delivery = match delivery {
+            // Carries the delivery alongside its channel
+            Ok(Some(delivery)) => delivery,
+            // The consumer got canceled
+            Ok(None) => return,
+            // Carries the error and is always followed by Ok(None)
+            Err(error) => {
+                dbg!("Failed to consume queue message {}", error);
+                return;
+            }
+        };
 
-        {
-            // send channel
-            let channel_send = Rabbitmq::channel(&conn).await;
-            // info!(state=?conn.status().state());
+        // Do something with the delivery data (The message payload)
+        info!(message=?delivery, "consumer received message");
 
-            // create the hello queue
-            let _ = Rabbitmq::queue(&channel_send, "hello").await;
-
-            // info!(state=?conn.status().state());
-
-            info!("will consume");
-            Rabbitmq::consume(&conn, "hello", "my_consumer").await;
-
-            // info!(state=?conn.status().state());
-
-            info!("will publish");
-            let payload = b"Hello world!";
-            let confirm = Rabbitmq::publish(&channel_send, "", "hello", payload).await;
-
-            assert_eq!(confirm, Confirmation::NotRequested);
-
-            // info!(state=?conn.status().state());
-        }
-
-        conn.run().expect("conn.run");
+        delivery
+            .ack(BasicAckOptions::default())
+            .await.expect("Failed to ack send_webhook_event message");
     });
 
+
+    for _ in 0..10 {
+        let _ = Rabbitmq::send(&channel, "", "queue_test", b"Hello world!").await;
+    }
+
+    // std::future::pending::<()>().await;
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
 
     Ok(())
 }
