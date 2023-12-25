0bb333ae77b29a7009ab1fc1520b191bb2cb2120
jason <jason@jaasun.com>
2023-12-25 18:57:33 +0800

    referral service: rabbitmq

--- microservices/referral/infras/src/config/rabbitmq.rs
+++ microservices/referral/infras/src/config/rabbitmq.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 use amqprs::BasicProperties;
 use amqprs::callbacks::{DefaultChannelCallback, DefaultConnectionCallback};
-use amqprs::channel::{BasicConsumeArguments, BasicPublishArguments, QueueBindArguments, QueueDeclareArguments};
+use amqprs::channel::{BasicConsumeArguments, BasicPublishArguments, Channel, QueueBindArguments, QueueDeclareArguments};
 use amqprs::connection::{Connection, OpenConnectionArguments};
 use amqprs::consumer::DefaultConsumer;
 
@@ -11,20 +11,36 @@ pub struct Rabbitmq;
 
 impl Rabbitmq {
     pub async fn connection() -> Connection {
+        // open a connection to RabbitMQ server
         let connection = Connection::open(&OpenConnectionArguments::new(
-            "localhost",
-            5672,
-            "rabbit",
-            "rabbitpassword",
-        ))
-            .await
-            .unwrap();
-        connection
-            .register_callback(DefaultConnectionCallback)
-            .await
-            .unwrap();
+            "localhost", 5672, "rabbit", "rabbitpassword"
+        )).await.unwrap();
+        connection.register_callback(DefaultConnectionCallback).await.unwrap();
         connection
     }
+
+    pub async fn channel(connection: &Connection) -> Channel {
+        // open a channel on the connection
+        let channel = connection.open_channel(None).await.unwrap();
+        channel.register_callback(DefaultChannelCallback).await.unwrap();
+        channel
+    }
+
+    pub async fn consume(channel: &Channel, queue_name: &str, consumer_tag: &str) -> String {
+        let args = BasicConsumeArguments::new(&queue_name, consumer_tag);
+        channel.basic_consume(DefaultConsumer::new(args.no_ack), args).await.unwrap()
+    }
+
+    pub async fn publish(channel: &Channel, exchange_name: &str, routing_key: &str, content: Vec<u8>) -> () {
+        let args = BasicPublishArguments::new(exchange_name, routing_key);
+        channel.basic_publish(BasicProperties::default(), content, args).await.unwrap();
+    }
+
+    pub async fn bind(channel: &Channel, queue_name: &str, routing_key: &str, exchange_name: &str) -> () {
+        channel.queue_declare(QueueDeclareArguments::durable_client_named(queue_name, )).await.unwrap().unwrap();
+        channel.queue_bind(QueueBindArguments::new(&queue_name, exchange_name, routing_key, )).await.unwrap();
+    }
+
 }
 
 
@@ -45,43 +61,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // open a connection to RabbitMQ server
     let connection = Rabbitmq::connection().await;
-
     // open a channel on the connection
-    let channel = connection.open_channel(None).await.unwrap();
-    channel
-        .register_callback(DefaultChannelCallback)
-        .await
-        .unwrap();
-
-    // declare a durable queue
-    let (queue_name, _, _) = channel
-        .queue_declare(QueueDeclareArguments::durable_client_named(
-            "hello.examples.basic",
-        ))
-        .await
-        .unwrap()
-        .unwrap();
+    let channel = Rabbitmq::channel(&connection).await;
 
     // bind the queue to exchange
+    let queue_name = "hello.examples.basic";
     let routing_key = "hello.example";
     let exchange_name = "hello";
-    channel
-        .queue_bind(QueueBindArguments::new(
-            &queue_name,
-            exchange_name,
-            routing_key,
-        ))
-        .await
-        .unwrap();
+
+    let _ = Rabbitmq::bind(&channel, &queue_name, routing_key, exchange_name).await;
 
     //////////////////////////////////////////////////////////////////////////////
     // start consumer with given name
-    let args = BasicConsumeArguments::new(&queue_name, "example_basic_pub_sub");
+    let r = Rabbitmq::consume(&channel, queue_name, "example_basic_pub_sub").await;
 
-    channel
-        .basic_consume(DefaultConsumer::new(args.no_ack), args)
-        .await
-        .unwrap();
 
     //////////////////////////////////////////////////////////////////////////////
     // publish message
@@ -95,12 +88,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     ).into_bytes();
 
     // create arguments for basic_publish
-    let args = BasicPublishArguments::new(exchange_name, routing_key);
-
-    channel
-        .basic_publish(BasicProperties::default(), content, args)
-        .await
-        .unwrap();
+    Rabbitmq::publish(&channel, exchange_name, routing_key, content).await;
 
     // keep the `channel` and `connection` object from dropping before pub/sub is done.
     // channel/connection will be closed when drop.
