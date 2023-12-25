4d3b5ad37b864ef1ddfa8171288684dea1e62e66
jason <jason@jaasun.com>
2023-12-25 19:06:37 +0800

    referral service: callback

--- microservices/referral/infras/src/config/rabbitmq.rs
+++ microservices/referral/infras/src/config/rabbitmq.rs
@@ -1,10 +1,55 @@
 use std::time::Duration;
-use amqprs::BasicProperties;
-use amqprs::callbacks::{DefaultChannelCallback, DefaultConnectionCallback};
-use amqprs::channel::{BasicConsumeArguments, BasicPublishArguments, Channel, QueueBindArguments, QueueDeclareArguments};
-use amqprs::connection::{Connection, OpenConnectionArguments};
+use amqprs::{
+    callbacks::{ChannelCallback, ConnectionCallback},
+    channel::Channel, connection::{Connection, OpenConnectionArguments},
+    Ack, BasicProperties, Cancel, Close, CloseChannel, Nack, Return,
+};
+use amqprs::channel::{BasicConsumeArguments, BasicPublishArguments, QueueBindArguments, QueueDeclareArguments};
 use amqprs::consumer::DefaultConsumer;
+use async_trait::async_trait;
 
+////////////////////////////////////////////////////////////////////////////////
+type Result<T> = std::result::Result<T, amqprs::error::Error>;
+
+////////////////////////////////////////////////////////////////////////////////
+struct ExampleConnectionCallback;
+
+#[allow(unused_variables, /* template */)]
+#[async_trait]
+impl ConnectionCallback for ExampleConnectionCallback {
+    async fn close(&mut self, connection: &Connection, close: Close) -> Result<()> {
+        Ok(())
+    }
+
+    async fn blocked(&mut self, connection: &Connection, reason: String) {}
+    async fn unblocked(&mut self, connection: &Connection) {}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+struct ExampleChannelCallback;
+
+#[allow(unused_variables, /* template */)]
+#[async_trait]
+impl ChannelCallback for ExampleChannelCallback {
+    async fn close(&mut self, channel: &Channel, close: CloseChannel) -> Result<()> {
+        Ok(())
+    }
+    async fn cancel(&mut self, channel: &Channel, cancel: Cancel) -> Result<()> {
+        Ok(())
+    }
+    async fn flow(&mut self, channel: &Channel, active: bool) -> Result<bool> {
+        Ok(true)
+    }
+    async fn publish_ack(&mut self, channel: &Channel, ack: Ack) {}
+    async fn publish_nack(&mut self, channel: &Channel, nack: Nack) {}
+    async fn publish_return(
+        &mut self,
+        channel: &Channel,
+        ret: Return,
+        basic_properties: BasicProperties,
+        content: Vec<u8>,
+    ) {}
+}
 
 #[derive(Debug)]
 pub struct Rabbitmq;
@@ -12,17 +57,16 @@ pub struct Rabbitmq;
 impl Rabbitmq {
     pub async fn connection() -> Connection {
         // open a connection to RabbitMQ server
-        let connection = Connection::open(&OpenConnectionArguments::new(
-            "localhost", 5672, "rabbit", "rabbitpassword"
-        )).await.unwrap();
-        connection.register_callback(DefaultConnectionCallback).await.unwrap();
+        let args = OpenConnectionArguments::new("localhost", 5672, "rabbit", "rabbitpassword");
+        let connection = Connection::open(&args).await.unwrap();
+        connection.register_callback(ExampleConnectionCallback).await.unwrap();
         connection
     }
 
     pub async fn channel(connection: &Connection) -> Channel {
         // open a channel on the connection
         let channel = connection.open_channel(None).await.unwrap();
-        channel.register_callback(DefaultChannelCallback).await.unwrap();
+        channel.register_callback(ExampleChannelCallback).await.unwrap();
         channel
     }
 
@@ -37,15 +81,14 @@ impl Rabbitmq {
     }
 
     pub async fn bind(channel: &Channel, queue_name: &str, routing_key: &str, exchange_name: &str) -> () {
-        channel.queue_declare(QueueDeclareArguments::durable_client_named(queue_name, )).await.unwrap().unwrap();
-        channel.queue_bind(QueueBindArguments::new(&queue_name, exchange_name, routing_key, )).await.unwrap();
+        channel.queue_declare(QueueDeclareArguments::durable_client_named(queue_name)).await.unwrap().unwrap();
+        channel.queue_bind(QueueBindArguments::new(&queue_name, exchange_name, routing_key)).await.unwrap();
     }
-
 }
 
 
 #[tokio::test]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     use tokio::time::sleep;
     use tracing_subscriber::{EnvFilter, fmt};
     use tracing_subscriber::layer::SubscriberExt;
@@ -73,7 +116,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     //////////////////////////////////////////////////////////////////////////////
     // start consumer with given name
-    let r = Rabbitmq::consume(&channel, queue_name, "example_basic_pub_sub").await;
+    let _ = Rabbitmq::consume(&channel, queue_name, "example_basic_pub_sub").await;
 
 
     //////////////////////////////////////////////////////////////////////////////
