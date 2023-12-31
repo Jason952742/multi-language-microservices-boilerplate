b5cbd5da8d3ad4ed34300371f13e362aa3ef34b8
jason <jason@jaasun.com>
2024-01-01 01:23:28 +0800

    c-portal service: mongo crud

--- microservices/c-portal/shared/src/datasource/mongo.rs
+++ microservices/c-portal/shared/src/datasource/mongo.rs
@@ -25,9 +25,10 @@ impl MongoPool {
         }).await
     }
 
-    async fn create_item<T: Serialize>(collection: Collection<T>, item: T) -> Result<(), mongodb::error::Error> {
-        let m = collection.insert_one(item, None).await?;
-        Ok(())
+    async fn create_item<T: Serialize>(collection: Collection<T>, item: T) -> Result<ObjectId, mongodb::error::Error> {
+        let result = collection.insert_one(item, None).await?;
+        let inserted_id = result.inserted_id.as_object_id().unwrap().clone();
+        Ok(inserted_id)
     }
 
     async fn find_item_by_id<T>(collection: Collection<T>, id: ObjectId) -> Result<Option<T>, mongodb::error::Error>
@@ -76,73 +77,70 @@ async fn main() -> Result<(), mongodb::error::Error> {
     let client = MongoPool::conn().await;
 
     // Parameterize our collection with the model.
-    let coll = client.database("items").collection::<Book>("in_stock");
-
-    let item = Book {
-        id: ObjectId::new(),
-        title: "helloworld".to_string(),
-        author: "George Orwell".to_string(),
-    };
-    coll.insert_one(item.clone(), None).await?;
-
-    let filter = doc! { "_id": item.id };
-    let found_item = coll.find_one(filter, None).await?;
-    println!("Found Item: {:?}", found_item);
-
-    let new_item = Book {
-        id: item.id,
-        title: "Wochao Item".to_string(),
-        ..item
-    };
-    let filter = doc! { "_id": item.id };
-    let update = doc! { "$set": <Book as Into<Bson>>::into(new_item) };
-    coll
-        .update_one(filter, update, UpdateOptions::builder().upsert(Some(false)).build())
-        .await?;
-
-    // Query the books in the collection with a filter and an option.
-    let filter = doc! { "author": "George Orwell" };
-    let find_options = FindOptions::builder().sort(doc! { "title": 1 }).build();
-    let mut cursor = coll.find(filter, find_options).await?;
-    // Iterate over the results of the cursor.
-    while let Some(book) = tokio_stream::StreamExt::try_next(&mut cursor).await? {
-        println!("found: {:?}", book);
-    }
-
-    let filter = doc! { "_id": item.id };
-    coll.delete_one(filter, None).await?;
-    let filter = doc! { "_id": item.id };
-    let found_item = coll.find_one(filter, None).await?;
-    println!("Found Item: {:?}", found_item);
-
-    // let collection = client.database("items").collection::<Book>("sell");
+    // let coll = client.database("items").collection::<Book>("in_stock");
     //
-    // let book = Book {
+    // let item = Book {
     //     id: ObjectId::new(),
-    //     title: "Example Item".to_string(),
-    //     author: "John".to_string(),
+    //     title: "helloworld".to_string(),
+    //     author: "George Orwell".to_string(),
     // };
-    // MongoPool::create_item(collection.clone(), book.clone()).await?;
+    // coll.insert_one(item.clone(), None).await?;
     //
-    // let found_item = MongoPool::find_item_by_id(collection.clone(), book.id).await?;
+    // let filter = doc! { "_id": item.id };
+    // let found_item = coll.find_one(filter, None).await?;
     // println!("Found Item: {:?}", found_item);
     //
-    // let updated_item = Book {
-    //     id: book.id,
-    //     title: "Updated Item".to_string(),
-    //     ..book
+    // let new_item = Book {
+    //     id: item.id,
+    //     title: "Wochao Item".to_string(),
+    //     ..item
     // };
-    // MongoPool::update_item(collection.clone(), book.id, updated_item.clone()).await?;
+    // let filter = doc! { "_id": item.id };
+    // let update = doc! { "$set": <Book as Into<Bson>>::into(new_item) };
+    // coll
+    //     .update_one(filter, update, UpdateOptions::builder().upsert(Some(false)).build())
+    //     .await?;
     //
-    // let filter = doc! { "author": "John"};
-    // let find_options = None;
-    // let found_items = find_books(collection.clone(), filter, find_options).await?;
-    // println!("Found Items: {:?}", found_items);
+    // // Query the books in the collection with a filter and an option.
+    // let filter = doc! { "author": "George Orwell" };
+    // let find_options = FindOptions::builder().sort(doc! { "title": 1 }).build();
+    // let mut cursor = coll.find(filter, find_options).await?;
+    // // Iterate over the results of the cursor.
+    // while let Some(book) = tokio_stream::StreamExt::try_next(&mut cursor).await? {
+    //     println!("found: {:?}", book);
+    // }
     //
-    // MongoPool::delete_item(collection.clone(), book.id).await?;
-    //
-    // let found_item = MongoPool::find_item_by_id(collection.clone(), book.id).await?;
+    // let filter = doc! { "_id": item.id };
+    // coll.delete_one(filter, None).await?;
+    // let filter = doc! { "_id": item.id };
+    // let found_item = coll.find_one(filter, None).await?;
     // println!("Found Item: {:?}", found_item);
+
+    let collection = client.database("items").collection::<Book>("sell");
+
+    let book = Book {
+        id: ObjectId::new(),
+        title: "Example Item".to_string(),
+        author: "John".to_string(),
+    };
+    let created_id = MongoPool::create_item(collection.clone(), book.clone()).await?;
+    println!("Created Item id: {:?}", created_id);
+
+    let found_item = MongoPool::find_item_by_id(collection.clone(), book.id).await?;
+    println!("Found Item: {:?}", found_item);
+
+    let updated_item = Book { id: book.id, title: "Updated Item".to_string(), ..book };
+    MongoPool::update_item(collection.clone(), book.id, updated_item.clone()).await?;
+
+    let filter = doc! { "author": "John"};
+    let find_options = None;
+    let found_items = find_books(collection.clone(), filter, find_options).await?;
+    println!("Found Items: {:?}", found_items);
+
+    MongoPool::delete_item(collection.clone(), book.id).await?;
+
+    let found_item = MongoPool::find_item_by_id(collection.clone(), book.id).await?;
+    println!("Found Item: {:?}", found_item);
 
     Ok(())
 }
