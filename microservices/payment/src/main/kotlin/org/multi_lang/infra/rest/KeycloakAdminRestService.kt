bed8fe3171ad5e82b5335aa7f6c644feb2e35808
jason <jason@jaasun.com>
2024-01-02 19:39:35 +0800

    payment service: update folder

--- microservices/payment/src/main/kotlin/org/multi_lang/infra/rest/KeycloakAdminRestService.kt
+++ microservices/payment/src/main/kotlin/org/multi_lang/infra/rest/KeycloakAdminRestService.kt
@@ -1,6 +1,5 @@
 package org.multi_lang.infra.service
 
-import io.smallrye.mutiny.Uni
 import jakarta.ws.rs.*
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
 import jakarta.ws.rs.core.Response
