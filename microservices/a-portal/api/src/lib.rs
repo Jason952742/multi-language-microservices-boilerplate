07c578595ce982962029e1ea8a580efdf3f54ff3
jason <jason@jaasun.com>
2024-01-08 14:02:01 +0800

    a-portal service: curd

--- microservices/a-portal/api/src/lib.rs
+++ microservices/a-portal/api/src/lib.rs
@@ -1,24 +1,22 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::fairing::{self, AdHoc};
 use rocket::form::{Context, Form};
 use rocket::fs::{relative, FileServer};
 use rocket::request::FlashMessage;
 use rocket::response::{Flash, Redirect};
 use rocket::{Build, Request, Rocket};
 use rocket_dyn_templates::Template;
-use service::{Mutation, Query};
 use serde_json::json;
+use shared::bson::doc;
+use shared::mongo::MongoPool;
+use shared::{Config, to_object_id};
 
-use migration::MigratorTrait;
-use sea_orm_rocket::{Connection, Database};
+use crate::domain::entities::post;
+use crate::infra::repositories::{PostDbMutation, PostDbQuery};
 
-mod pool;
-use pool::Db;
-
-pub use entity::post;
-pub use entity::post::Entity as Post;
+mod infra;
+mod domain;
 
 const DEFAULT_POSTS_PER_PAGE: u64 = 5;
 
@@ -28,12 +26,12 @@ async fn new() -> Template {
 }
 
 #[post("/", data = "<post_form>")]
-async fn create(conn: Connection<'_, Db>, post_form: Form<post::Model>) -> Flash<Redirect> {
-    let db = conn.into_inner();
+async fn create(post_form: Form<post::FormModel>) -> Flash<Redirect> {
+    let conn = MongoPool::conn().await.clone();
 
     let form = post_form.into_inner();
 
-    Mutation::create_post(db, form)
+    PostDbMutation::create_post(&conn, form.into())
         .await
         .expect("could not insert post");
 
@@ -41,30 +39,22 @@ async fn create(conn: Connection<'_, Db>, post_form: Form<post::Model>) -> Flash
 }
 
 #[post("/<id>", data = "<post_form>")]
-async fn update(
-    conn: Connection<'_, Db>,
-    id: i32,
-    post_form: Form<post::Model>,
-) -> Flash<Redirect> {
-    let db = conn.into_inner();
+async fn update(id: &str, post_form: Form<post::FormModel>) -> Flash<Redirect> {
+    let conn = MongoPool::conn().await.clone();
+    let id = to_object_id(id).unwrap();
 
     let form = post_form.into_inner();
 
-    Mutation::update_post_by_id(db, id, form)
+    PostDbMutation::update_post_by_id(&conn, id, form.into())
         .await
-        .expect("could not update post");
+        .expect("could not edit post");
 
     Flash::success(Redirect::to("/"), "Post successfully edited.")
 }
 
 #[get("/?<page>&<posts_per_page>")]
-async fn list(
-    conn: Connection<'_, Db>,
-    page: Option<u64>,
-    posts_per_page: Option<u64>,
-    flash: Option<FlashMessage<'_>>,
-) -> Template {
-    let db = conn.into_inner();
+async fn list(page: Option<u64>, posts_per_page: Option<u64>, flash: Option<FlashMessage<'_>>) -> Template {
+    let conn = MongoPool::conn().await.clone();
 
     // Set page number and items per page
     let page = page.unwrap_or(1);
@@ -73,13 +63,16 @@ async fn list(
         panic!("Page number cannot be zero");
     }
 
-    let (posts, num_pages) = Query::find_posts_in_page(db, page, posts_per_page)
+    let filter = doc! { };
+    let find_options = None;
+    let (num_pages, posts) = PostDbQuery::find_posts_in_page(&conn, filter, find_options, page, posts_per_page)
         .await
         .expect("Cannot find posts in page");
+    let posts: Vec<post::FormModel> = posts.into_iter().map(|x| post::FormModel::from(x)).collect();
 
     Template::render(
         "index",
-        json! ({
+        json!({
             "page": page,
             "posts_per_page": posts_per_page,
             "num_pages": num_pages,
@@ -90,26 +83,31 @@ async fn list(
 }
 
 #[get("/<id>")]
-async fn edit(conn: Connection<'_, Db>, id: i32) -> Template {
-    let db = conn.into_inner();
+async fn edit(id: &str) -> Template {
+    let conn = MongoPool::conn().await.clone();
+    let id = to_object_id(id).unwrap();
 
-    let post: Option<post::Model> = Query::find_post_by_id(db, id)
+    let post: post::Model = PostDbQuery::find_post_by_id(&conn, id)
         .await
-        .expect("could not find post");
+        .expect("could not find post")
+        .unwrap_or_else(|| panic!("could not find post with id {id}"));
+
+    let post = post::FormModel::from(post);
 
     Template::render(
         "edit",
-        json! ({
+        json!({
             "post": post,
         }),
     )
 }
 
 #[delete("/<id>")]
-async fn delete(conn: Connection<'_, Db>, id: i32) -> Flash<Redirect> {
-    let db = conn.into_inner();
+async fn delete(id: String) -> Flash<Redirect> {
+    let conn = MongoPool::conn().await.clone();
+    let id = to_object_id(id.clone()).unwrap();
 
-    Mutation::delete_post(db, id)
+    PostDbMutation::delete_post(&conn, id)
         .await
         .expect("could not delete post");
 
@@ -117,12 +115,12 @@ async fn delete(conn: Connection<'_, Db>, id: i32) -> Flash<Redirect> {
 }
 
 #[delete("/")]
-async fn destroy(conn: Connection<'_, Db>) -> Result<(), rocket::response::Debug<String>> {
-    let db = conn.into_inner();
+async fn destroy() -> Result<(), rocket::response::Debug<String>> {
+    let conn = MongoPool::conn().await.clone();
 
-    Mutation::delete_all_posts(db)
+    PostDbMutation::delete_all_post(&conn)
         .await
-        .map_err(|e| e.to_string())?;
+        .expect("could not delete post");
 
     Ok(())
 }
@@ -131,41 +129,24 @@ async fn destroy(conn: Connection<'_, Db>) -> Result<(), rocket::response::Debug
 pub fn not_found(req: &Request<'_>) -> Template {
     Template::render(
         "error/404",
-        json! ({
+        json!({
             "uri": req.uri()
         }),
     )
 }
 
-async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
-    let conn = &Db::fetch(&rocket).unwrap().conn;
-    let _ = migration::Migrator::up(conn, None).await;
-    Ok(rocket)
-}
 
-#[tokio::main]
-async fn start() -> Result<(), rocket::Error> {
+pub async fn start(config: Config) -> anyhow::Result<()> {
     rocket::build()
-        .attach(Db::init())
-        .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
         .mount("/", FileServer::from(relative!("/static")))
-        .mount(
-            "/",
-            routes![new, create, delete, destroy, list, edit, update],
-        )
+        .mount("/", routes![new, create, delete, destroy, list, edit, update])
         .register("/", catchers![not_found])
         .attach(Template::fairing())
         .launch()
         .await
-        .map(|_| ())
-}
-
-pub fn main() {
-    let result = start();
+        .map(|_| ())?;
 
     println!("Rocket: deorbit.");
 
-    if let Some(err) = result.err() {
-        println!("Error: {err}");
-    }
+    Ok(())
 }
