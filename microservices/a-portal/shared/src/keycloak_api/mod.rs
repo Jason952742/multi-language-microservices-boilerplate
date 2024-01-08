25cfa2103a56b96b968d5642ac02bc84ef96fd6e
jason <jason@jaasun.com>
2024-01-08 12:49:19 +0800

    a-portal service: add shared

---
+++ microservices/a-portal/shared/src/keycloak_api/mod.rs
@@ -0,0 +1,4 @@
+pub mod admin;
+pub mod keycloak;
+pub mod openid;
+pub mod urls;
---
+++ microservices/a-portal/shared/src/keycloak_api/openid.rs
@@ -0,0 +1,35 @@
+use reqwest::header::{HeaderValue, CONTENT_TYPE};
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Token {
+    pub access_token: String,
+    pub token_type: String,
+    pub session_state: String,
+    pub scope: String,
+}
+
+pub async fn get_token(path: &str, payload: serde_json::Value) -> Result<Token, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let k_res = client
+        .post(path)
+        .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+        .form(&payload)
+        .send()
+        .await?.error_for_status()?;
+    k_res.json().await
+}
+
+pub async fn introspect_token(
+    path: &str,
+    payload: serde_json::Value,
+) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let k_res = client
+        .post(path)
+        .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+        .form(&payload)
+        .send()
+        .await?.error_for_status()?;
+    k_res.text().await
+}
